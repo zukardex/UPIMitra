4d543973fad5d425955cbe79004c1ce55487739f
Mohammed Suhail K M <zenzenram@gmail.com>
2023-12-06 20:15:00 +0530

    Update tesseract

---
+++ tesseract.py
@@ -0,0 +1,11 @@
+import pytesseract
+import argparse
+import cv2
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image to be OCR'd")
+args = vars(ap.parse_args())
+image = cv2.imread(args["image"])
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+text = pytesseract.image_to_string(image)
+#print(text)
